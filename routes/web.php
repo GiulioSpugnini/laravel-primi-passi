f02cb4df293e32eab975755b578f6f9da97e37f2
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-03-15 09:41:45 +0100

    fixed home Route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
+Route::get('/', function () {
     $title = 'Hello World';
     return view('home', ['title' => $title]);
 });
